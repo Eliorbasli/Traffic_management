ab5425da2eb9690857d0385a44f1fe4f63294c5a
eliorbasli <eliorbasli@gmail.com>
2024-08-18 19:29:40 +0300

    create User response

--- backend/app/schemas/user_schema.py
+++ backend/app/schemas/user_schema.py
@@ -1,6 +1,16 @@
+from typing import Optional
+from uuid import UUID
 from pydantic import BaseModel , EmailStr , Field
 
 class UserAuth(BaseModel):
     username: str = Field(... , min_length=5 , max_length=50 , description="user username")
     password: str = Field(... , min_length=5, max_length=24 , description="user password")
     email: EmailStr = Field( ... , description="user email")
+    
+class UserOut(BaseModel):
+    user_id: UUID
+    username: str
+    email: str
+    first_name: Optional[str]
+    last_name: Optional[str]
+    disabled: bool = False
