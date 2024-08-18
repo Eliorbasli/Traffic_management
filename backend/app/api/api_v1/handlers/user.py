db3c7ace0adb32d8ff380d04e4918d81034fb0c0
eliorbasli <eliorbasli@gmail.com>
2024-08-18 19:31:14 +0300

    /login end point and functions created

--- backend/app/api/api_v1/handlers/user.py
+++ backend/app/api/api_v1/handlers/user.py
@@ -1,14 +1,14 @@
 from fastapi import APIRouter, HTTPException , status
-from app.schemas.user_schema import UserAuth
-from app.services.user_service import userService
+from app.schemas.user_schema import UserAuth , UserOut
+from app.services.user_service import UserService
 
 import pymongo
 user_router = APIRouter()
 
-@user_router.post('/create' , summary= " Create new user")
+@user_router.post('/create', summary="Create new user" , response_model=UserOut)
 async def create_user(data: UserAuth):
     try:
-        return await userService.creaate_user(data)
+        return await UserService.creaate_user(data)
     except pymongo.errors.DuplicateKeyError:
         raise HTTPException(
             status_code= status.HTTP_400_BAD_REQUEST,
