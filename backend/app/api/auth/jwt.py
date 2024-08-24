ab59f59dab609b9b7e81907a30953a8a0fbabfe2
eliorbasli <eliorbasli@gmail.com>
2024-08-24 15:09:52 +0300

    Fix sysntex error

--- backend/app/api/auth/jwt.py
+++ backend/app/api/auth/jwt.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status , Body
 from fastapi.security import OAuth2PasswordRequestForm
 from typing import Any
 from app.services.user_service import UserService
@@ -7,6 +7,10 @@ from app.schemas.auth_schema import TokenSchema
 from app.schemas.user_schema import UserOut
 from app.models.user_model import User
 from app.api.deps.user_deps import get_current_user
+from app.core.config import settings
+from app.schemas.auth_schema import TokenPayload
+from pydantic import ValidationError
+from jose import jwt
 
 auth_router = APIRouter()
 
@@ -19,7 +23,6 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends()) -> Any:
             detail="Incorrect email or password"
         )
         
-    #create access and refresh tokens
     return {
         "access_token" : create_access_token(user.user_id),
         "refresh_token": create_refresh_token(user.user_id)
@@ -28,5 +31,27 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends()) -> Any:
 @auth_router.post('/test-token', summary ="Test if the access token is valid" ,response_model=UserOut )
 async def test(user: User = Depends(get_current_user)):
     return user
-    
-    
+
+@auth_router.post('/refresh' , summary="Refresh token" , response_model=TokenSchema)
+async def refresh_token(refresh_token: str = Body (...)):
+    try:
+        payload = jwt.decode(
+            refresh_token, settings.JWT_REFRESH_SECRET_KEY, algorithms=[settings.ALGORITHM]
+        )
+        token_data = TokenPayload(**payload)
+    except (jwt.JWTError, ValidationError):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Invalied token",
+            headers={"WWW-Authentication" : "Bearer"}
+        )
+    user = await UserService.get_user_by_id(token_data.sub)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Invalied token for user",
+        )
+    return {
+        "access_token" : create_access_token(user.user_id),
+        "refresh_token" : create_refresh_token(user.user_id)
+    }
