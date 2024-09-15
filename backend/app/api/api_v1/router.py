6f3694e47fe207751546af727449b6e0e170af3d
eliorbasli <eliorbasli@gmail.com>
2024-09-15 23:28:47 +0300

    Add traffic map to router

--- backend/app/api/api_v1/router.py
+++ backend/app/api/api_v1/router.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter
 from app.api.api_v1.handlers import user
+from app.api.api_v1.handlers import traffic_map
 from app.api.auth.jwt import auth_router
+
 
 router = APIRouter()
 
 router.include_router(user.user_router , prefix='/users' , tags=['users'])
+router.include_router(traffic_map.traffic_map_router , prefix='/traffic_map' , tags=['users'])
 router.include_router(auth_router , prefix='/auth' , tags=['auth'])
 
