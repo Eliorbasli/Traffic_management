a23ec771d74b17ffc5482a8b6c796a20e635ec20
eliorbasli <eliorbasli@gmail.com>
2024-09-22 00:26:09 +0300

    Update Graph model

--- backend/app/models/graph_model.py
+++ backend/app/models/graph_model.py
@@ -1,15 +1,38 @@
-from typing import Optional
-from datetime import datetime
+from typing import List, Dict, Tuple
 from uuid import UUID, uuid4
-from beanie import Document, Indexed
-from pydantic import Field, EmailStr
-from typing import Dict, List, Tuple
+from beanie import Document, Indexed ,before_event, Link, Replace , Insert
+from datetime import datetime
+from pydantic import BaseModel, Field
 
+from .user_model import User
 class Graph(Document):
-    graph_id: UUID = Field(default_factory=uuid4 , unique=True)
+    graph_id: UUID = Field(default_factory=uuid4, unique=True)
+    username:str = Indexed(unique=False)
     nodes: List[str]
-    edegs: Dict[str, List[Tuple[str, int]]] # Each edge connects to a node with a distance
+    edges: tuple[str, List[List]]  # Adjust type as needed
+    # edges: List[Tuple[str,str]]  # Adjust type as needed
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime = Field(default_factory=datetime.now)
+    owner : Link[User]
+
+    def __repr__(self) -> str:
+        return f"<Graph {self.graph_id}>"
     
-    class Settings:
-        collection = "graphs"
+    def __str__(self) -> str:
+        return self.name
     
+    def __hash__(self) -> int:
+        return hash(self.name)
+    
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, Graph):
+            return self.graph_id == other.graph_id
+        return False
+
+
+@before_event([Replace , Insert])
+def update_update_at(self):
+    self.updated_at = datetime.now()
+    
+class Collection:
+    name = "graphs"
