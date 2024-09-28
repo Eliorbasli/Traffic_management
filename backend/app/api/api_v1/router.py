432d57494850453dfb688abc5aecdad78e574ce5
eliorbasli <eliorbasli@gmail.com>
2024-09-28 22:26:21 +0300

    Add car router with /cars prefix

--- backend/app/api/api_v1/router.py
+++ backend/app/api/api_v1/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from app.api.api_v1.handlers import user
 from app.api.api_v1.handlers import graph
+from app.api.api_v1.handlers import car
 from app.api.auth.jwt import auth_router
 
 
@@ -9,4 +10,5 @@ router = APIRouter()
 router.include_router(user.user_router , prefix='/users' , tags=['users'])
 router.include_router(graph.graph_router , prefix='/graph' , tags=['users'])
 router.include_router(auth_router , prefix='/auth' , tags=['auth'])
+router.include_router(car.car_router , prefix='/cars' , tags=['cars'])
 
