247607caca01be8eaf0e93533d59fae9d35a64c0
eliorbasli <eliorbasli@gmail.com>
2024-10-10 13:11:28 +0300

    Create new function get_graph_by_id

--- backend/app/services/graph_service.py
+++ backend/app/services/graph_service.py
@@ -2,16 +2,18 @@ from typing import List
 from app.models.user_model import User
 from app.models.graph_model import Graph
 from app.schemas.graph_schema import GraphCreate
+from beanie import PydanticObjectId
 
 class GraphService:
     @staticmethod
     async def list_graphs(user: User):
-        graphs = await Graph.find(Graph.owner.id == user.user_id).to_list()
+        user_object_id = PydanticObjectId(user.id)
+        graphs = await Graph.find(Graph.owner.id == user_object_id).to_list()
         return graphs
 
 
     @staticmethod
-    async def create_graph(user: User , data: GraphCreate ) -> Graph:
+    async def create_graph(user: User , data: GraphCreate) -> Graph:
         graph_data = data.model_dump()
         
         edges = graph_data.get("edges")
@@ -24,5 +26,11 @@ class GraphService:
     @staticmethod
     async def get_graph_by_name(name: str) -> List[Graph]:
         graphs = await Graph.find({"name": name}).to_list()
+        
+        return graphs
+    
+    @staticmethod
+    async def get_graph_by_id(graph_id: str) -> List[Graph]:
+        graphs = await Graph.find({"graph_id": graph_id}).to_list()
         
         return graphs
